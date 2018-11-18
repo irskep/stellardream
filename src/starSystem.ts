31160ab303137b2f5cc14c0ce36182d4a47ff42f
Steve Johnson <steve@steveasleep.com>
2018-11-17 18:13:39 -0800

    Bunch of comments about binary star systems

--- src/starSystem.ts
+++ src/starSystem.ts
@@ -14,12 +14,29 @@ export class StarSystem {
 
         this.stars = [new Star(alea)];
         
-        if (alea() > 0.5) {
-            this.stars.push(new Star(alea));
-        }
+        /*
+          Roughly 44% of star systems have two stars. The stars orbit each
+          other at distances of "zero-ish" to 1 light year. Alpha Centauri,
+          for example, has Proxima Centauri at 15,000 AU (~0.23 light years).
 
-        this.stars.sort((a, b) => {
-            return b.mass - a.mass;
-        });
+          This model will only look at "close binaries" (hand-wavingly
+          estimated at half of binary systems), and say their planets are in
+          orbit of both stars simultaneously. Other binaries will be treated
+          like separate star systems. Research shows that even non-close
+          binaries make planetary orbits eccentric over time (billions of
+          years), but this would have no effect on colonization potential by
+          humans (I suppose) except as it relates to the development of
+          human-relevant life.
+         */
+        if (alea() > 0.22) {
+            this.stars.push(new Star(alea));
+
+            // One strategy for generating the second star would be to force
+            // it to be smaller than the first, but it's simpler to just
+            // generate them independently and sort by mass.
+            this.stars.sort((a, b) => {
+                return b.mass - a.mass;
+            });
+        }
     }
 }
